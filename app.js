40cf7b4c1a97e0033dea27b842438840c825468f
Pratik Sahu <sahupratik30@gmail.com>
2021-06-28 20:56:59 +0530

    initial commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const score = document.querySelector(".score");
 const gameArea = document.querySelector(".game_area");
 const messageArea = document.querySelector(".message_area");
-let player = { speed: 8 };
+let player = { speed: 5 };
 //Declaring object to store keys
 let keys = {
   ArrowUp: false,
@@ -10,7 +10,12 @@ let keys = {
   ArrowLeft: false,
   ArrowRight: false,
 };
-
+//Creating array for enemy cars
+const enemyCars = [
+  "images/enemy1.png",
+  "images/enemy2.png",
+  "images/enemy3.png",
+];
 //Function for key down event
 function keyDown(e) {
   e.preventDefault();
@@ -24,6 +29,7 @@ function keyUp(e) {
 //Function to play game
 function playGame() {
   moveLines();
+  moveEnemy();
   let car = document.querySelector(".car");
   let road = gameArea.getBoundingClientRect();
   console.log("Game started");
@@ -57,15 +63,28 @@ function moveLines() {
     line.style.top = line.y + "px";
   });
 }
+//Function to move enemy cars
+function moveEnemy() {
+  let enemies = document.querySelectorAll(".enemy");
+  enemies.forEach(function (enemy) {
+    if (enemy.y > 750) {
+      enemy.y = -300;
+      enemy.style.left = Math.floor(Math.random() * 320) + "px";
+    }
+    enemy.y += player.speed;
+    enemy.style.top = enemy.y + "px";
+  });
+}
 //Function to start game
 function startGame() {
+  player.start = true;
   messageArea.classList.add("hide");
   gameArea.classList.remove("hide");
-  player.start = true;
   window.requestAnimationFrame(playGame);
   let car = document.createElement("div");
   car.setAttribute("class", "car");
   gameArea.appendChild(car);
+  //Creating Lines on the road
   for (let x = 0; x < 5; x++) {
     let roadLine = document.createElement("div");
     roadLine.setAttribute("class", "lines");
@@ -75,6 +94,18 @@ function startGame() {
   }
   player.x = car.offsetLeft;
   player.y = car.offsetTop;
+  //Creating enemy cars
+  for (let x = 0; x < 3; x++) {
+    let enemy = document.createElement("div");
+    enemy.setAttribute("class", "enemy");
+    enemy.y = (x + 1) * 350 * -1;
+    enemy.style.top = enemy.y + "px";
+    let randomEnemy = Math.floor(Math.random() * 3);
+    enemy.style.backgroundImage = `url(${enemyCars[randomEnemy]})`;
+    enemy.style.left = Math.floor(Math.random() * 320) + "px";
+    console.log(enemy.style.left);
+    gameArea.appendChild(enemy);
+  }
 }
 //Key event listeners
 document.addEventListener("keydown", keyDown);
